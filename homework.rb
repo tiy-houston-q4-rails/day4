26886403c8d475860ad30caba7a7eba443bc8d12
Jonathan Birkholz <rookieone@gmail.com>
2014-09-25 13:44:19 -0500

    First pass at homework

---
+++ homework.rb
@@ -0,0 +1,19 @@
+# Define a Robot class
+# A robot has a name
+
+# A robot should have a method called 'say_hi' and it should return "Hi!"
+
+# A robot should have a method called 'say_name' and it should return
+# "My name is X" where X is the robot's name
+
+# Define a BendingUnit class
+# A BendingUnit inherits from Robot
+# A bending unit has a method called 'bend'
+# The bend method has one argument 'object_to_bend'
+# The bend method should put to the console "Bend X!" where X is object_to_bend
+
+# Define an ActorUnit class
+# An ActorUnit inherits from Robot
+# An ActorUnit has a method called 'change_name'
+# The 'change_name' method accepts an argument 'new_name'
+# The 'change_name' method changes the name property of Robot to 'new_name'
